a9474a0895be9172967e46e7ed412e07cae57c4a
sabbirsr <rakibhasansabbir@yahoo.com>
2017-10-20 15:12:03 +0600

    Find out the student with the longest name

--- Collections Demo/src/main/java/bd/ac/seu/collections/Main.java
+++ Collections Demo/src/main/java/bd/ac/seu/collections/Main.java
@@ -1,8 +1,15 @@
 package bd.ac.seu.collections;
 
+/**
+ *@author Rakib Hasan Sabbir
+ */
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    private List<Student> studentList;
     public Main() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -16,13 +23,31 @@ public class Main {
 
             String query = "select * from student";
             ResultSet resultSet = statement.executeQuery(query);
-
+            studentList = new ArrayList<>();
             while (resultSet.next()){
                 int studentId = resultSet.getInt("studentId");
                 String studentName = resultSet.getString("studentName");
-                System.out.println(studentId + ", "+ studentName);
+                Student student = new Student(studentId,studentName);
+                studentList.add(student);
             }
+/*
+            for (int i = 0; i<studentList.size();i++){
+                System.out.println(studentList.get(i));
+            }
+*/
+/*
+            for (Student student: studentList){
+                System.out.println(student);
+            }
+*/
+//Find out the student with the longest name
 
+            Student maxlengthNameStudent = studentList.get(0);
+            for(Student student:studentList){
+                if (student.getStudentName().length() > maxlengthNameStudent.getStudentName().length())
+                    maxlengthNameStudent = student;
+            }
+            System.out.println("max length of student is: " + maxlengthNameStudent);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
