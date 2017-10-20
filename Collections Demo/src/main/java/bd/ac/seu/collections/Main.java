bf4ad92b6c4972d779fd2e49a6452252264e48f9
sabbirsr <rakibhasansabbir@yahoo.com>
2017-10-20 11:15:31 +0600

    create connection and fetch data for collectionDemo

--- Collections Demo/src/main/java/bd/ac/seu/collections/Main.java
+++ Collections Demo/src/main/java/bd/ac/seu/collections/Main.java
@@ -1,7 +1,36 @@
 package bd.ac.seu.collections;
 
+import java.sql.*;
+
 public class Main {
+    public Main() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            final String DBURL = "jdbc:mysql://localhost/predictdb";
+            final String USERNAME = "root";
+            final String PASSWORD = "rakib";
+
+            Connection connection = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+            Statement statement = connection.createStatement();
+
+            String query = "select * from student";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()){
+                int studentId = resultSet.getInt("studentId");
+                String studentName = resultSet.getString("studentName");
+                System.out.println(studentId + ", "+ studentName);
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Is it working?");
+        new Main();
     }
 }
